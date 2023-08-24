11e2d2a2f9325b4fa47fb323d08bc237e8c536cd
FX. Mario Jevta Prasetio <fxmariojevta@gmail.com>
2023-08-24 20:36:42 +0700

    (01_Arrays) feat: add sum_of_array

---
+++ 01_Arrays/src/sum_of_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n {0};
+    cout << "Enter the number of elements" << endl;
+    cin >> n;
+
+    vector<int> a(n, 0);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    int sum {0};
+    for (int i = 0; i < n; ++i) {
+        sum += a[i];
+    }
+
+    cout << sum << endl;
+}
