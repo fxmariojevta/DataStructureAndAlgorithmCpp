8ad3720abdb8223c2620edf02df50c1ebfb5a3d5
FX. Mario Jevta Prasetio <fxmariojevta@gmail.com>
2023-08-26 17:29:39 +0700

    (01_Arrays) feat: add swap_2_numbers

---
+++ 01_Arrays/src/swap_2_numbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a {0};
+    int b {0};
+
+    cin >> a >> b;
+    cout << "Value before swap:" << endl;
+    cout << "Value a :" << a << endl;
+    cout << "Value b :" << b << endl;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    // swap(a, b)
+
+    cout << "Value after swap:" << endl;
+    cout << "Value a :" << a << endl;
+    cout << "Value b :" << b << endl;
+
+    return 0;
+}
